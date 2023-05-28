340a69aa271e021b907a265a51fe2123183e9bfb
Alexandr Naum <mfq07k9cz@gmail.com>
2023-05-28 07:00:40 +0300

    updated componets to class

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Header } from './Header'
+import {Header} from './Header'
 
 import './App.css'
 
-function App() {
-  return (
-    <div className='App'>
-      <Header />
-      Hello, samurai! Let's go!
-      <h1>Go!</h1>
-    </div>
-  )
+class App extends React.Component {
+    render() {
+        return (
+            <div className='App'>
+                <Header/>
+                Hello, samurai! Let's go!
+                <h1>Go!</h1>
+            </div>
+        )
+    }
 }
 
 export default App
